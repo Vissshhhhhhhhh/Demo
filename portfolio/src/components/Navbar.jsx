d3fe546e1ffcd2ba9f5202d180735511d47ea32f
Vissshhhhhhhhh <viswanathpaarthiban1@gmail.com>
2025-11-04 12:33:09 +0530

    portfolio commit

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import "./Navbar.css";
 import Logo from "../assets/logo.webp";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from '../pages/Home.jsx'
-import About from '../pages/About.jsx'
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
@@ -13,23 +11,14 @@ export default function Navbar() {
           <img src={Logo} alt="Logo" />
         </div>
 
-       <Router>
-         <nav className="navbar-menu">
-          <Link to="/">Home</Link>|{" "}
-          <Link to="/about">About</Link>|{" "}
-          <Link to="/projects">Projects</Link>|{" "}
-          <Link to="/exp">Experience</Link>|{" "}
-          <Link to="/contact">Contact</Link>|{" "}
-
-          <Routes>
-            <Route path="/" element={<Home />}  />
-            <Route path="/about" element={<About />} />
-            {/* <Route />
-            <Route />
-            <Route /> */}
-          </Routes>
-        </nav>
-       </Router>
+         <div className="navbar-menu">
+          <Link to="/Home">Home</Link>{" "}
+          <Link to="/about">About</Link>{" "}
+          <Link to="/projects">Projects</Link>{" "}
+          <Link to="/exp">Experience</Link>{" "}
+          <Link to="/contact">Contact</Link>{" "}
+        </div>
+        
       </nav>
     </>
   );
